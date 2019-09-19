5c9a88b10342d7c64b44b90472a27ba93c693d9a
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-09-19 13:02:12 +0100

    add errorMessage

--- user-onboarding/src/Component/Form.js
+++ user-onboarding/src/Component/Form.js
@@ -7,14 +7,17 @@ export default function Form () {
             <label>
                 Name
                 <Field name='name' type='text' placeholder='Enter your Name' />
+                <ErrorMessage name='name' component='div' />
             </label>
             <label>
                 Email
                 <Field name='email' type='text' placeholder='Enter your Email' />
+                <ErrorMessage name='email' component='div' />
             </label>
             <label>
                 Password
                 <Field name='password' type='text' placeholder='Input your Password' />
+                <ErrorMessage name='password' component='div' />
             </label>
             <label>
                 Terms of service
