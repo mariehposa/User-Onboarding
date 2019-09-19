bac4b521e7ec80fc426a7dd5a0e992b43e7d6f33
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-09-19 12:58:52 +0100

    create formik form

--- user-onboarding/src/Component/Form.js
+++ user-onboarding/src/Component/Form.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
+
 export default function Form () {
     return (
         <div>
-            
+            <label>
+                Name
+                <Field name='name' type='text' placeholder='Enter your Name' />
+            </label>
+            <label>
+                Email
+                <Field name='email' type='text' placeholder='Enter your Email' />
+            </label>
+            <label>
+                Password
+                <Field name='password' type='text' placeholder='Input your Password' />
+            </label>
+            <label>
+                Terms of service
+                <Field name='checkbox' type='checkbox' />
+            </label>
         </div>
     );
 }
