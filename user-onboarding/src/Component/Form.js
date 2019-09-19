cbbfda0d3fe5c8a17369a0b1fb49500f2bf5db40
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-09-19 13:28:00 +0100

    implement form validation

--- user-onboarding/src/Component/Form.js
+++ user-onboarding/src/Component/Form.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as yup from 'yup';
 
-
+const validation = yup.object().shape({
+    name: yup.string().required('Please enter correct name!'),
+    email: yup.string().required('Please enter valid email!'),
+    password: yup.string().required('Please input correct password!'),
+})
 
 export default function UserForm () {
     return (
@@ -15,16 +19,19 @@ export default function UserForm () {
                             <Field name='name' type='text' placeholder='Enter your Name' />
                             <ErrorMessage name='name' component='div' />
                         </label>
+                        <br/>
                         <label>
                             Email
                             <Field name='email' type='text' placeholder='Enter your Email' />
                             <ErrorMessage name='email' component='div' />
                         </label>
+                        <br/>
                         <label>
                             Password
                             <Field name='password' type='text' placeholder='Input your Password' />
                             <ErrorMessage name='password' component='div' />
                         </label>
+                        <br/>
                         <label>
                             Terms of service
                             <Field name='checkbox' type='checkbox' />
